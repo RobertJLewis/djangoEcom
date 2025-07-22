533a8f3c4e48948eb56fe44b8c88cef4a2e8e85e
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-07-22 19:03:25 +0100

    blue print on products

--- cataloge/admin.py
+++ cataloge/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Catagory, ProductImage
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'parant')
+    search_fields = ('name',)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('title', 'price', 'category', 'is_active', 'modified_at')
+    search_fields = ('title', 'description')
+    inlines = [ProductImageInline]
+    list_filter = ('catagory', 'is_active')
+
+admin.site.register(Catagory, CategoryAdmin)
+
+
---
+++ cataloge/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+app_name = 'cataloge'
+urlpatterns = [
+    path('', views.ProductListView.as_view(), name='index'),
+    path('category/<slug:slug>/', views.CategoryView.as_view(), name='category_detail'),
+    path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+]
+
+
