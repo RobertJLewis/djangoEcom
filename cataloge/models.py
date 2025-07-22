47a149840011f03f1384e0e10c799e2d6b6dd395
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-07-22 18:41:57 +0100

    creating catalogle blue print

--- cataloge/models.py
+++ cataloge/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+class timeStampedMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Product(timeStampedMixin):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Catagory(timeStampedMixin):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    parant = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('cataloge:category_detail', kwargs={'slug': self.slug})
+    
+class ProductImage(timeStampedMixin):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    alt_text = models.CharField(max_length=255, blank=True, null=True)
+    is_primary = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['-is_primary', 'id'] 
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+    
+
+# maybe add stock record class in future 
