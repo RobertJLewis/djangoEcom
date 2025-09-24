cbdb36964b0d865c1d14b4ad0ccd0cd50884acfd
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-09-24 19:12:14 +0100

    - Updated template to use MEDIA_URL for gogLogo.webp - Ensured media folder is served in development via urls.py - Clarified difference between static and media assets

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_summernote',
 
     # Allauth
     'allauth',
@@ -156,7 +157,6 @@ USE_TZ = True
 # Static & Media
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'products'
+app_name = 'catalog'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
